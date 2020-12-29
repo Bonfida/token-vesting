ca5780a909c700218abe6e200e14462bccffb37e
ellttBen <elliott@bonfida.com>
2020-12-29 15:25:51 +0100

    Make lock instruction safe. Instruction naming refactor pending.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 - Simple vesting contract (SVC) that allows you to deposit X SPL tokens that are unlocked to a specified public key at a certain block height/ slot.
 - Unlocking works by pushing a permissionless crank on the contract that moves the tokens to the pre-specified address
 - Token Address should be derived from https://spl.solana.com/associated-token-account
-- 'Vesting Schedule contract' - A contract containing an array of the SVC's that can be used to develop arbitrary- vesting schedules.
+- 'Vesting Schedule contract' - A contract containing an array of the SVC's that can be used to develop arbitrary- vesting schedules. (?)
 - Tooling to easily setup vesting schedule contracts
 - Recipient address should be modifiable by the owner of the current recipient key
 - Implementation should be a rust spl compatible program, plus client side javascript bindings that include a CLI- interface. Rust program should be unit tested and fuzzed.
--- program/src/instruction.rs
+++ program/src/instruction.rs
@@ -1,6 +1,6 @@
 use crate::error::VestingError;
 
-use solana_program::{program_error::ProgramError, msg};
+use solana_program::{program_error::ProgramError, msg, pubkey::Pubkey};
 
 use std::mem::size_of;
 use std::convert::TryInto;
@@ -9,12 +9,16 @@ use std::convert::TryInto;
 #[derive(Clone, Debug, PartialEq)]
 pub enum VestingInstruction {
     Init {
-        seeds: [u8; 32]
+        seeds: [u8; 32],
+        amount: u64,
+        release_height: u64,
+        mint_address: Pubkey
     },
     Lock {
         seeds: [u8; 32],
         amount: u64,
-        release_height: u64
+        release_height: u64,
+        mint_address: Pubkey
     },
     Unlock {
         seeds: [u8; 32]
@@ -30,13 +34,7 @@ impl VestingInstruction {
         let (&tag, rest) = input.split_first().ok_or(InvalidInstruction)?;
         msg!("Parsed tag : {:?}", tag);
         Ok(match tag {
-            0 => {
-                let seeds:[u8; 32] = rest
-                    .get(..32)
-                    .and_then(|slice| slice.try_into().ok()).unwrap();
-                VestingInstruction::Init{ seeds }
-            }
-            1 => {
+            0 | 1 => {
                 let seeds:[u8; 32] = rest
                     .get(..32)
                     .and_then(|slice| slice.try_into().ok()).unwrap();
@@ -51,8 +49,19 @@ impl VestingInstruction {
                 .and_then(|slice| slice.try_into().ok())
                 .map(u64::from_le_bytes)
                 .ok_or(InvalidInstruction)?;
+
+                let mint_address = rest
+                .get(48..80)
+                .and_then(|slice| slice.try_into().ok())
+                .map(Pubkey::new)
+                .ok_or(InvalidInstruction)?;
                 msg!("Parsed release_height");
-                Self::Lock { seeds , amount, release_height }
+                match tag {
+                    0 => Self::Init { seeds , amount, release_height, mint_address },
+                    1 => Self::Lock { seeds , amount, release_height, mint_address },
+                    _ => unreachable!()
+                }
+
             }
             2 => {
                 let seeds:[u8; 32] = rest
@@ -66,15 +75,19 @@ impl VestingInstruction {
     pub fn pack(&self) -> Vec<u8> {
         let mut buf = Vec::with_capacity(size_of::<Self>());
         match self {
-            &Self::Init { seeds} => {
+            &Self::Init { seeds, amount, release_height , mint_address} => {
                 buf.push(0);
                 buf.extend_from_slice(&seeds);
+                buf.extend_from_slice(&amount.to_le_bytes());
+                buf.extend_from_slice(&release_height.to_le_bytes());
+                buf.extend_from_slice(&mint_address.to_bytes());
             }
-            &Self::Lock { seeds, amount, release_height } => {
+            &Self::Lock { seeds, amount, release_height , mint_address} => {
                 buf.push(1);
                 buf.extend_from_slice(&seeds);
                 buf.extend_from_slice(&amount.to_le_bytes());
                 buf.extend_from_slice(&release_height.to_le_bytes());
+                buf.extend_from_slice(&mint_address.to_bytes());
             }
             &Self::Unlock { seeds} => {
                 buf.push(2);
@@ -91,16 +104,19 @@ mod test {
 
     #[test]
     fn test_instruction_packing(){
+        let mint_address = Pubkey::new_unique();
         let check = VestingInstruction::Lock {
             seeds: [50u8;32],
             amount: 42,
-            release_height: 250
+            release_height: 250,
+            mint_address: mint_address.clone()
         };
         let mut expected = Vec::from([1]);
         let seeds = [50u8;32];
         let data = [42, 0, 0, 0, 0, 0, 0, 0, 250, 0, 0, 0, 0, 0, 0, 0];
         expected.extend_from_slice(&seeds);
         expected.extend_from_slice(&data);
+        expected.extend_from_slice(&mint_address.to_bytes());
         let packed = check.pack();
         assert_eq!(expected, packed);
         let unpacked = VestingInstruction::unpack(&packed).unwrap();
