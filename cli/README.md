7031f049bb397dd60de735ef22f2fbe580d5851a
Lcchy <lucas@bonfida.com>
2021-01-04 16:44:44 +0100

    Adapt CLI create and unlock to core updates

--- cli/README.md
+++ cli/README.md
@@ -1,11 +1,29 @@
-## Testing Keys
+## Testing Keys for the devnet
 
-source: 8mbbe4MjEyMnuqp83dY6hNLL9LhrYNqqMnnLFaa7tyNe
-seed: velvet filter job industry lyrics invite actual surface tuition palm bamboo congress
+KEYS: 
 
-destination: HAGYPxGe7RE62qegZrs7eMeQzi7HxxeBxfkGp5jc57zx
-seed: toilet eye poet hungry protect shallow panther lonely base usage flush subject
+token_mint: 3wmMWPDkSdKd697arrGWYJ1q4QL1jwGxnANUyXqSV9vC
 
-CMD
+source_owner: ~/.config/solana/id.json
+source_token: EWrBFuSdmMC3wQKvWaCUTLbDhQT3Mpmw2CVViK4P5Xk2
 
-RUST_BACKTRACE=1 ./target/debug/vesting-contract-cli --program_id Hz4Lk33fvrL8T1KM17kMgHyKvzWrtsuj54mCUFMZYP7j --url https://devnet.solana.com create-svc --amount 2 --destination 8mbbe4MjEyMnuqp83dY6hNLL9LhrYNqqMnnLFaa7tyNe --seed 12345123451234512345123451234512 --source ~/.config/solana/id.json
+destination_owner: ~/.config/solana/id_dest.json
+dest_token: An2CVh3tm13Ld1EKKfiFE6udNFjmZNTSAzM4QzAdqCVZ
+
+CMDS (don't forget the url):
+
+solana-keygen new --outfile ~/.config/solana/id.json
+solana-keygen new --outfile ~/.config/solana/id_dest.json
+solana airdrop 10 --url https://devnet.solana.com ~/.config/solana/id.json
+solana deploy ../program/target/deploy/token_vesting.so --url https://devnet.solana.com
+
+spl-token create-token
+spl-token create-account MINT --url https://devnet.solana.com KEYPAIR
+spl-token mint 3wmMWPDkSdKd697arrGWYJ1q4QL1jwGxnANUyXqSV9vC 100 --url https://devnet.solana.com KEYPAIR
+
+RUST_BACKTRACE=1 ./target/debug/vesting-contract-cli --url https://devnet.solana.com --program_id G19pYmA6dn8jhDaGai2kNPcj9e5bB4jnXLgT8kTE9BCq --mint_address 3wmMWPDkSdKd697arrGWYJ1q4QL1jwGxnANUyXqSV9vC create-svc --amount 2 --destination An2CVh3tm13Ld1EKKfiFE6udNFjmZNTSAzM4QzAdqCVZ --seed 11345123451234512345123451234512 --source ~/.config/solana/id.json --source_token_address EWrBFuSdmMC3wQKvWaCUTLbDhQT3Mpmw2CVViK4P5Xk2
+
+
+LINKS:
+
+https://spl.solana.com/token
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,4 +1,4 @@
-use spl_associated_token_account::get_associated_token_address;
+use spl_associated_token_account::{get_associated_token_address, create_associated_token_account};
 use token_vesting::instruction::{VestingInstruction, create, unlock};
 use clap::{
     crate_description, crate_name, crate_version, value_t, App, AppSettings, Arg, SubCommand
@@ -7,54 +7,84 @@ use solana_client::{
     rpc_client::RpcClient,
 };
 use solana_clap_utils::{input_parsers::{keypair_of, lamports_of_sol, pubkey_of, value_of}, input_validators::{is_amount, is_keypair, is_pubkey, is_url, is_parsable}};
-use solana_sdk::{signature::Signer, signature::Keypair, transaction::Transaction};
-use solana_program::{instruction::{AccountMeta, Instruction}, msg, pubkey::Pubkey, system_program};
+use solana_sdk::{self, system_instruction, signature::Signer, signature::{Keypair, keypair_from_seed}, transaction::Transaction};
+use solana_program::{instruction::{AccountMeta, Instruction}, msg, pubkey::Pubkey, system_program, rent::Rent, sysvar};
+use spl_token;
 use std::convert::TryInto;
 
 // Lock the vesting contract
 fn command_create_svc(
     rpc_client: RpcClient,
     program_id: Pubkey,
-    mut vesting_seed: [u8;32],
+    vesting_seed: [u8;32],
     source_token_owner: Keypair,
+    source_token_pubkey: Pubkey, // TODO make it Option, if None, use associated
     destination_token_pubkey: Pubkey,
     mint_address: Pubkey,
     vesting_amount: u64
 ) {
     // Find the non reversible public key for the vesting contract via the seed    
-    let (vesting_pubkey, bump) = Pubkey::find_program_address(&[&vesting_seed[..31]], &program_id);
-    vesting_seed[31] = bump;
+    // let (vesting_pubkey, bump) = Pubkey::find_program_address(&[&vesting_seed[..31]], &program_id);
+    // vesting_seed[31] = bump;
+    let vesting_keypair = keypair_from_seed(&vesting_seed).unwrap();
+    let vesting_pubkey = vesting_keypair.pubkey();
     msg!("Vesting account pubkey: {:?}", vesting_pubkey);
 
-    let source_token_pubkey = get_associated_token_address(
-        &source_token_owner.pubkey(), 
+    let vesting_token_pubkey = get_associated_token_address(
+        &vesting_pubkey, 
         &mint_address
     );
 
-    let decimals = rpc_client
-        .get_token_account(&source_token_pubkey)
-        .unwrap()
-        .unwrap()
-        .token_amount.decimals;
+    // let decimals = rpc_client
+    //     .get_token_account(&source_token_pubkey)
+    //     .unwrap()
+    //     .unwrap()
+    //     .token_amount.decimals;
 
-    let create_instruction = create(
-        &program_id,
-        &vesting_pubkey,
-        &source_token_pubkey,
-        &destination_token_pubkey,
-        &mint_address,
-        vesting_amount,
-        0,
-        vesting_seed
-    ).unwrap();
+    let instructions = [
+        // Create the vesting account
+        // This is the fundamental problem of program account creation. 
+        // How to create the account without a private key as the 
+        // create_with_seed address generation does not match the find_programm_adress ?
+        // (Maybe create and assign, loose the private kez and keep the seeds?)
+        // Leaving this for later.
+        system_instruction::create_account(
+            &source_token_owner.pubkey(),
+            &vesting_pubkey,
+            // &source_token_owner.pubkey(),
+            // std::str::from_utf8(&vesting_seed).unwrap(),
+            Rent::default().minimum_balance(165),
+            165,
+            &program_id
+        ),
+        // Create and initiliaze the vesting token account
+        create_associated_token_account(
+            &source_token_owner.pubkey(),
+            &vesting_pubkey,
+            &mint_address
+        ),
+        create(
+            &program_id,
+            &spl_token::id(),
+            &vesting_pubkey,
+            &vesting_token_pubkey,
+            &source_token_owner.pubkey(),
+            &source_token_pubkey,
+            &destination_token_pubkey,
+            &mint_address,
+            vesting_amount,
+            0,
+            vesting_seed
+        ).unwrap()
+   ];
 
     let mut transaction = Transaction::new_with_payer(
-        &[create_instruction],
-        Some(&source_pubkey.pubkey()),
+        &instructions,
+        Some(&source_token_owner.pubkey()),
     );
 
     let recent_blockhash = rpc_client.get_recent_blockhash().unwrap().0;
-    transaction.sign(&[&source_pubkey], recent_blockhash);
+    transaction.sign(&[&source_token_owner, &vesting_keypair], recent_blockhash);
 
     rpc_client.send_transaction(&transaction).unwrap();
 }
@@ -62,22 +92,33 @@ fn command_create_svc(
 fn command_unlock_svc(
     rpc_client: RpcClient,
     program_id: Pubkey,
-    mut vesting_seed: [u8;32],
+    vesting_seed: [u8;32],
     source_pubkey: Keypair,
-    destination_pubkey: Pubkey,
+    destination_token_pubkey: Pubkey,
     mint_address: Pubkey,
     vesting_amount: u64
 ) {
     // Find the non reversible public key for the vesting contract via the seed    
-    let (vesting_pubkey, bump) = Pubkey::find_program_address(&[&vesting_seed[..31]], &program_id);
-    vesting_seed[31] = bump;
+    // let (vesting_pubkey, bump) = Pubkey::find_program_address(&[&vesting_seed[..31]], &program_id);
+    // vesting_seed[31] = bump;
+    let vesting_keypair = keypair_from_seed(&vesting_seed).unwrap();
+    let vesting_pubkey = vesting_keypair.pubkey();
     msg!("Vesting account pubkey: {:?}", vesting_pubkey);
+
+    let vesting_token_pubkey = get_associated_token_address(
+        &vesting_pubkey, 
+        &mint_address
+    );
+
+    //TODO initialize associated destination token account if none found
 
     let unlock_instruction = unlock(
         &program_id,
+        &spl_token::id(),
+        &sysvar::clock::id(),
         &vesting_pubkey,
-        &destination_pubkey,
-        &mint_address,
+        &vesting_token_pubkey,
+        &destination_token_pubkey,
         vesting_seed,
     ).unwrap();
 
@@ -122,9 +163,17 @@ fn main() {
                 .validator(is_pubkey)
                 .takes_value(true)
                 .help(
-                    "Specify the adress (publickey) of the program. \
-                     This may be a keypair file, the ASK keyword. \
-                     Defaults to the client keypair.",
+                    "Specify the address (public key) of the program.",
+                ),
+        )
+        .arg(
+            Arg::with_name("mint_address")
+                .long("mint_address")
+                .value_name("ADDRESS")
+                .validator(is_pubkey)
+                .takes_value(true)
+                .help(
+                    "Specify the adress (publickey) of the mint for the token that should be used.",
                 ),
         )
         .subcommand(SubCommand::with_name("create-svc").about("Create a new simple vesting contract")        
@@ -132,12 +181,10 @@ fn main() {
                 Arg::with_name("seed")
                     .long("seed")
                     .value_name("ADDRESS")
-                    // .validator(is_hash)
+                    // .validator(is_hash)  //TODO
                     .takes_value(true)
                     .help(
-                        "Specify the seed for the vesting contract. \
-                        This may be a keypair file, the ASK keyword. \
-                        Defaults to the client keypair.",
+                        "Specify the seed for the vesting contract.",
                     ),
             )        
             .arg(
@@ -153,8 +200,18 @@ fn main() {
                     ),
             )
             .arg(
+                Arg::with_name("source_token_address")
+                    .long("source_token_address")
+                    .value_name("ADDRESS")
+                    .validator(is_pubkey)
+                    .takes_value(true)
+                    .help(
+                        "Specify the source token account address.",
+                    ),
+            )     
+            .arg(
                 Arg::with_name("destination")
-                    .long("destination")
+                    .long("destination_token_address")
                     .value_name("ADDRESS")
                     .validator(is_pubkey)
                     .takes_value(true)
@@ -233,27 +290,32 @@ fn main() {
     let rpc_client = RpcClient::new(rpc_url);
 
     let program_id = pubkey_of(&matches, "program_id").unwrap();
+    let mint_address = pubkey_of(&matches, "mint_address").unwrap();
     
     // solana_logger::setup_with_default("solana=info");
     
     let _ = match matches.subcommand() {
         ("create-svc", Some(arg_matches)) => {
             let vesting_seed = (*String::as_bytes(&value_of(arg_matches, &"seed").unwrap())).try_into().unwrap();
-            let source_pubkey = keypair_of(arg_matches, "source").unwrap();
+            let source_keypair = keypair_of(arg_matches, "source").unwrap();
+            let source_token_pubkey = pubkey_of(arg_matches, "source_token_address").unwrap();
             let destination_pubkey = pubkey_of(arg_matches, "destination").unwrap();
             let vesting_amount = lamports_of_sol(arg_matches, "amount").unwrap();
             msg!("Program ID: {:?}", &program_id);
             msg!("Vesting Seed: {:?}", &vesting_seed);
-            msg!("Source Pubkey: {:?}", &source_pubkey);
+            msg!("Source Pubkey: {:?}", &source_keypair.pubkey());
             msg!("Destination Pubkey: {:?}", &destination_pubkey);
             msg!("Vesting Amount: {:?}", &vesting_amount);
             command_create_svc(
                 rpc_client,
                 program_id,
                 vesting_seed,
-                source_pubkey,
+                source_keypair,
+                source_token_pubkey,
                 destination_pubkey,
-                vesting_amount
+                mint_address,
+                vesting_amount,
+
             )
         }
         ("unlock-svc", Some(arg_matches)) => {
@@ -272,6 +334,7 @@ fn main() {
                 vesting_seed,
                 source_pubkey,
                 destination_pubkey,
+                mint_address,
                 vesting_amount
             )
         }
