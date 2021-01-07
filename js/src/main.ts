2ec99066ec8cf5d143bd6b5bfa3993c54ae955ec
dr497 <anand.ratiney@gmail.com>
2021-01-07 14:05:17 +0800

    Improve JS style and continue work.

--- js/src/main.ts
+++ js/src/main.ts
@@ -1,60 +1,99 @@
-import { Account, PublicKey, SystemProgram } from '@solana/web3.js';
-import { TOKEN_PROGRAM_ID, u64 } from '@solana/spl-token';
+import {
+  Account,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+} from '@solana/web3.js';
+import { TOKEN_PROGRAM_ID } from '@solana/spl-token';
 import { createInitInstruction, Schedule } from './instructions';
+import {
+  getDerivedSeed,
+  getAccountFromSeed,
+  connection,
+  account,
+  VESTING_PROGRAM_ID,
+  tokenPubkey,
+  destinationPubkey,
+  mintAddress,
+  schedule,
+  signTransactionInstructions,
+  walletSeed,
+} from './utils';
 
-
-const SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID: PublicKey = new PublicKey(
-    'ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL',
-  );
-  
 async function findAssociatedTokenAddress(
-    walletAddress: PublicKey,
-    tokenMintAddress: PublicKey
-    ): Promise<PublicKey> {
-    return (await PublicKey.findProgramAddress(
-        [
-            walletAddress.toBuffer(),
-            TOKEN_PROGRAM_ID.toBuffer(),
-            tokenMintAddress.toBuffer(),
-        ],
-        SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID
-    ))[0];
+  walletAddress: PublicKey,
+  tokenMintAddress: PublicKey,
+): Promise<PublicKey> {
+  return (
+    await PublicKey.findProgramAddress(
+      [
+        walletAddress.toBuffer(),
+        TOKEN_PROGRAM_ID.toBuffer(),
+        tokenMintAddress.toBuffer(),
+      ],
+      VESTING_PROGRAM_ID,
+    )
+  )[0];
 }
-
 
 async function create(
-    programId: PublicKey,
-    vestingSeed: Array<Buffer | Uint8Array>,
-    payer: Account,
-    source_token_owner: Account,
-    possible_source_token_pubkey: PublicKey | null,
-    destination_token_pubkey: PublicKey,
-    mint_address: PublicKey,
-    schedules: Array<Schedule>
-    ){
+  programId: PublicKey,
+  vestingSeed: Array<Buffer | Uint8Array>,
+  payer: Account,
+  sourceTokenOwner: Account,
+  possibleSourceTokenPubkey: PublicKey | null,
+  destinationTokenPubkey: PublicKey,
+  mintAddress: PublicKey,
+  schedules: Array<Schedule>,
+) {
+  // If no source token account was given, use the associated source account
+  if (possibleSourceTokenPubkey == null) {
+    possibleSourceTokenPubkey = await findAssociatedTokenAddress(
+      sourceTokenOwner.publicKey,
+      mintAddress,
+    );
+  }
 
-    // If no source token account was given, use the associated source account
-    if (possible_source_token_pubkey == null) {
-        possible_source_token_pubkey = await findAssociatedTokenAddress(
-            source_token_owner.publicKey, mint_address);
-    }
+  const numberOfSchedules = schedules.length;
 
-    const numberOfSchedules = new u64(schedules.length);
+  // Find the non reversible public key for the vesting contract via the seed
+  vestingSeed = vestingSeed.slice(0, 31);
+  const [pubkey, bump] = await PublicKey.findProgramAddress(
+    vestingSeed,
+    programId,
+  );
 
-    // Find the non reversible public key for the vesting contract via the seed    
-    let vestingPubkey = await PublicKey.createProgramAddress(vestingSeed, programId);
-    console.log("Vesting token account pubkey: ", vestingPubkey);
+  console.log('Vesting token account pubkey: ', pubkey.toBase58());
 
-    let instruction = [
-        createInitInstruction(
-            SystemProgram.programId,
-            programId,
-            payer.publicKey,
-            vestingPubkey,
-            vestingSeed,
-            schedules.length
-        )
-
-    ]
-
+  let instruction = [
+    createInitInstruction(
+      SystemProgram.programId,
+      programId,
+      payer.publicKey,
+      pubkey,
+      [Buffer.from(vestingSeed.toString() + bump, 'hex')],
+      schedules.length,
+    ),
+  ];
+  return instruction;
 }
+
+const test = async (): Promise<void> => {
+  const instructions = await create(
+    VESTING_PROGRAM_ID,
+    [Buffer.from('11111111111114512345123451234512', 'hex')],
+    account,
+    account,
+    tokenPubkey,
+    destinationPubkey,
+    mintAddress,
+    [schedule],
+  );
+  const signed = await signTransactionInstructions(
+    connection,
+    account,
+    instructions,
+  );
+};
+
+test();
---
+++ js/src/utils.ts
@@ -0,0 +1,107 @@
+// @ts-nocheck
+import BN from 'bn.js';
+import assert from 'assert';
+import nacl from 'tweetnacl';
+import * as bip32 from 'bip32';
+import {
+  Account,
+  Connection,
+  Transaction,
+  TransactionInstruction,
+  PublicKey,
+} from '@solana/web3.js';
+import { Schedule } from './instructions';
+
+export class Numberu64 extends BN {
+  /**
+   * Convert to Buffer representation
+   */
+  toBuffer() {
+    const a = super.toArray().reverse();
+    const b = Buffer.from(a);
+    if (b.length === 8) {
+      return b;
+    }
+    assert(b.length < 8, 'Numberu64 too large');
+
+    const zeroPad = Buffer.alloc(8);
+    b.copy(zeroPad);
+    return zeroPad;
+  }
+
+  /**
+   * Construct a Numberu64 from Buffer representation
+   */
+  static fromBuffer(buffer) {
+    assert(buffer.length === 8, `Invalid buffer length: ${buffer.length}`);
+    return new BN(
+      [...buffer]
+        .reverse()
+        .map(i => `00${i.toString(16)}`.slice(-2))
+        .join(''),
+      16,
+    );
+  }
+}
+
+// Connection
+
+const ENDPOINTS = {
+  mainnet: 'https://solana-api.projectserum.com',
+  devnet: 'https://devnet.solana.com',
+};
+
+export const connection = new Connection(ENDPOINTS.devnet);
+
+// For accounts imported from Sollet.io
+
+export const getDerivedSeed = (seed: Buffer) => {
+  const derivedSeed = bip32.fromSeed(seed).derivePath(`m/501'/0'/0/0`)
+    .privateKey;
+  return nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
+};
+
+export const getAccountFromSeed = (seed: Buffer) => {
+  const derivedSeed = bip32.fromSeed(seed).derivePath(`m/501'/0'/0/0`)
+    .privateKey;
+  return new Account(nacl.sign.keyPair.fromSeed(derivedSeed).secretKey);
+};
+
+// Test params
+export const VESTING_PROGRAM_ID: PublicKey = new PublicKey(
+  'Hj9R6bEfrULLNrApMsKCEaHR9QJ2JgRtM381xgYcjFmQ',
+);
+export const walletSeed = Buffer.from(
+  'cf556a77183c563b77986835d39d600a8d56998254d42d95888f91df9bb20fabc5da8e06f59a202bf23fb99e3cd10d2ea292437baa80d9d78c7e0f6f2eaf5621',
+  'hex',
+);
+export const account = getAccountFromSeed(walletSeed);
+export const tokenPubkey = new PublicKey(
+  '4PkZGUcaQoW7o138fUyn2xi1PfBNH2RFEavxyoKfJvtG',
+);
+export const mintAddress = new PublicKey(
+  'GAVRiTwa55gNrVZwsRzLGkCmLC1qvrFtUAfD1ARz5spP',
+);
+
+export const destinationPubkey = new PublicKey(
+  '4F9NzDF3Z1PbJizbGJdZ3KvQJMrkK1GEBaN6BVmnmkzG',
+);
+
+export const schedule: Schedule = {
+  releaseHeight: new Numberu64(29507188),
+  amount: new Numberu64(10),
+};
+
+// Sign stuff
+
+export const signTransactionInstructions = async (
+  connection: Connection,
+  account: Account,
+  txInstructions: Array<TransactionInstruction>,
+): Promise<string> => {
+  const tx = new Transaction();
+  tx.add(...txInstructions);
+  return await connection.sendTransaction(tx, [account], {
+    preflightCommitment: 'single',
+  });
+};
