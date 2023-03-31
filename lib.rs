070c4c6a5bac158a1b90747ac010a498f90dff03
RaDzikalbonie <114030306+RaDzikalbonie@users.noreply.github.com>
2023-03-31 12:41:59 +0100

    Add files via upload

--- lib.rs
+++ lib.rs
@@ -1,23 +1,53 @@
-#![doc = include_str!("../README.md")]
+use anchor_lang::prelude::*;
 
-#[cfg(feature = "parser")]
-mod parser;
-#[cfg(feature = "parser")]
-pub use crate::parser::*;
+use solana_security_txt::security_txt;
 
-pub const SECURITY_TXT_BEGIN: &str = "=======BEGIN SECURITY.TXT V1=======\0";
-pub const SECURITY_TXT_END: &str = "=======END SECURITY.TXT V1=======\0";
+#[cfg(not(feature = "no-entrypoint"))]
+security_txt! {
+    // Required fields
+    name: "Example",
+    project_url: "http://example.com",
+    contacts: "email:example@example.com,link:https://example.com/security,discord:example#1234",
+    policy: "https://github.com/solana-labs/solana/blob/master/SECURITY.md",
 
-#[macro_export]
-macro_rules! security_txt {
-    ($($name:ident: $value:expr),*) => {
-        #[cfg_attr(target_arch = "bpf", link_section = ".security.txt")]
-        #[allow(dead_code)]
-        #[no_mangle]
-        pub static security_txt: &str = concat! {
-            "=======BEGIN SECURITY.TXT V1=======\0",
-            $(stringify!($name), "\0", $value, "\0",)*
-            "=======END SECURITY.TXT V1=======\0"
-        };
-    };
+    // Optional Fields
+    preferred_languages: "en,de",
+    source_code: "https://github.com/example/example",
+    encryption: "
+-----BEGIN PGP PUBLIC KEY BLOCK-----
+Comment: Alice's OpenPGP certificate
+Comment: https://www.ietf.org/id/draft-bre-openpgp-samples-01.html
+
+mDMEXEcE6RYJKwYBBAHaRw8BAQdArjWwk3FAqyiFbFBKT4TzXcVBqPTB3gmzlC/U
+b7O1u120JkFsaWNlIExvdmVsYWNlIDxhbGljZUBvcGVucGdwLmV4YW1wbGU+iJAE
+ExYIADgCGwMFCwkIBwIGFQoJCAsCBBYCAwECHgECF4AWIQTrhbtfozp14V6UTmPy
+MVUMT0fjjgUCXaWfOgAKCRDyMVUMT0fjjukrAPoDnHBSogOmsHOsd9qGsiZpgRnO
+dypvbm+QtXZqth9rvwD9HcDC0tC+PHAsO7OTh1S1TC9RiJsvawAfCPaQZoed8gK4
+OARcRwTpEgorBgEEAZdVAQUBAQdAQv8GIa2rSTzgqbXCpDDYMiKRVitCsy203x3s
+E9+eviIDAQgHiHgEGBYIACAWIQTrhbtfozp14V6UTmPyMVUMT0fjjgUCXEcE6QIb
+DAAKCRDyMVUMT0fjjlnQAQDFHUs6TIcxrNTtEZFjUFm1M0PJ1Dng/cDW4xN80fsn
+0QEA22Kr7VkCjeAEC08VSTeV+QFsmz55/lntWkwYWhmvOgE=
+=iIGO
+-----END PGP PUBLIC KEY BLOCK-----
+",
+    auditors: "None",
+    acknowledgements: "
+The following hackers could've stolen all our money but didn't:
+- Neodyme
+"
 }
+
+declare_id!("H6EoGSe3xgMEsnjg8ADcXb5B8GF4gJXEesQhyMFz4ZxR");
+
+#[program]
+pub mod example_contract {
+    use super::*;
+
+    pub fn hello_world(_ctx: Context<HelloWorld>) -> Result<()> {
+        msg!("Hello World!");
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct HelloWorld {}
